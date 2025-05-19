9cf1bb11c322875400d52130d106183d29d5354e
mrMaekawa <mrmaekawa@gmail.com>
2025-05-18 23:10:41 -0300

    fix(chore): fix package files

--- scripts/build-lambdas.js
+++ scripts/build-lambdas.js
@@ -6,19 +6,23 @@ const lambdasList = path.resolve(__dirname, "lambdas.json");
 const lambdas = JSON.parse(fs.readFileSync(lambdasList, "utf8"));
 
 const buildLambda = async ({ name, entry }) => {
-  try {
-    await esbuild.build({
-      entryPoints: [entry],
-      bundle: true,
-      platform: "node",
-      target: "node18",
-      outfile: `dist/lambdas/${name}/index.js`,
-      minify: true, 
-    });
-    console.log(`✔️ Build da função ${name} concluído!`);
-  } catch (error) {
-    console.error(`❌ Erro ao compilar ${name}:`, error.message);
-  }
+    try {
+        await esbuild.build({
+            entryPoints: [entry],
+            bundle: true,
+            platform: "node",
+            target: "node18",
+            outfile: `dist/lambdas/${name}/index.js`,
+            minify: true, 
+            external: [], 
+            sourcemap: true,
+            packages: "external",
+            logLevel: "info",
+        });
+        console.log(`✔️ Build da função ${name} concluído!`);
+    } catch (error) {
+        console.error(`❌ Erro ao compilar ${name}:`, error.message);
+    }
 };
 
 lambdas.forEach(buildLambda);
