089438861f32e917ae8bcf944b2688fe30040c98
mrMaekawa <mrmaekawa@gmail.com>
2025-05-19 01:56:47 -0300

    fix(chore): fix wrong redis variables

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,5 +1,10 @@
 import { RedisClientType, createClient } from 'redis';
 
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT || '6379';
+const redisUser = process.env.REDIS_USER || 'default';
+const redisPassword = process.env.REDIS_PASSWORD;
+
 export class RedisClient {
     private static instance: RedisClientType;
 
@@ -8,8 +13,9 @@ export class RedisClient {
     public static getInstance(): RedisClientType {
         if (!RedisClient.instance) {
             //const redisUrl = `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-            const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-      
+            //const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+            const redisUrl = `redis://${redisUser}:${redisPassword}@${redisHost}:${redisPort}`;
+
             RedisClient.instance = createClient({
                 url: redisUrl
             });
